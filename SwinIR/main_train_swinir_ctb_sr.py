3ebd3f177de033f3d4eeab428f7132001f72643c
WeijieChen2017 <weijiechen1997@gmail.com>
2021-10-29 02:04:37 -0500

    Good morning!

--- SwinIR/main_train_swinir_ctb_sr.py
+++ SwinIR/main_train_swinir_ctb_sr.py
@@ -90,7 +90,7 @@ def main():
             print("--->",cube_x_path,"<---", end="")
             cube_x_data = np.load(cube_x_path)
             cube_y_data = np.load(cube_y_path)
-            assert cube_x_data.shape == cube_y_data.shape
+            # assert cube_x_data.shape == cube_y_data.shape
             len_z = cube_x_data.shape[1]
             case_loss = np.zeros((len_z//args.batch))
             input_list = list(range(len_z))
