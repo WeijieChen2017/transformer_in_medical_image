bf81af39d58e7540a8fb554a885dde64e0e5027f
WeijieChen2017 <weijiechen1997@gmail.com>
2021-10-31 00:24:46 -0500

    Good morning!

--- SwinIR/unet/unet_parts.py
+++ SwinIR/unet/unet_parts.py
@@ -71,12 +71,10 @@ class Up(nn.Module):
 class OutConv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(OutConv, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
-        self.linear = nn.Linear(out_channels, 1)
+        self.conv = nn.Conv2d(in_channels, in_channels, kernel_size=1)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1)
 
     def forward(self, x):
-        print(x.size())
         x = self.conv(x)
-        print(x.size())
-        x = self.linear(x)
+        x = self.conv1(x)
         return x
