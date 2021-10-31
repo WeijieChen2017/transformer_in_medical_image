a9871ee2e0ceabafb1c384de963db281fba7b56a
WeijieChen2017 <weijiechen1997@gmail.com>
2021-10-31 16:33:41 -0500

    Good morning!

--- SwinIR/main_test_unet_b.py
+++ SwinIR/main_test_unet_b.py
@@ -19,10 +19,10 @@ np.random.seed(seed=813)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--gpu_ids', type=str, default="3", help='Use which GPU to train')
-    parser.add_argument('--folder_X_te', type=str, default="./MR2CT_S/X/test/", help='input folder of T1MAP PET images')
-    parser.add_argument('--folder_Y_te', type=str, default="./MR2CT_S/Y/test/", help='input folder of BRAVO images')
-    parser.add_argument('--weights_path', type=str, default='./saved_models/MR2CT_S_model_best_014.pth')
+    parser.add_argument('--gpu_ids', type=str, default="7", help='Use which GPU to train')
+    parser.add_argument('--folder_X_te', type=str, default="./MR2CT_B_UNET/test_from_previous/", help='input folder of T1MAP PET images')
+    parser.add_argument('--folder_Y_te', type=str, default="./MR2CT_B_UNET/Y/test/", help='input folder of BRAVO images')
+    parser.add_argument('--weights_path', type=str, default='./MR2CT_B_UNET/model_best_100.pth')
     args = parser.parse_args()
 
     gpu_list = ','.join(str(x) for x in args.gpu_ids)
@@ -30,6 +30,10 @@ def main():
     print('export CUDA_VISIBLE_DEVICES=' + gpu_list)
 
     device = torch.device('cuda' if  torch.cuda.is_available() else 'cpu')
+
+    for path in ["./MR2CT_B_UNET/pred/"]:
+        if not os.path.exists(path):
+            os.mkdir(path)
 
     print(f'loading model from {args.weights_path}')
     model = torch.load(args.weights_path)
@@ -45,54 +49,42 @@ def main():
     for cnt_X, X_path in enumerate(X_list):
 
         cube_x_path = X_path
-        cube_y_path = X_path.replace("X", "Y")
-        print("->",cube_x_path,"<-", end="")
+        cube_y_path = args.folder_Y_te+os.path.basename(X_path)
+        print("->",cube_x_path, "<->", cube_y_path, "<-",end="")
         cube_x_data = np.load(cube_x_path)
         cube_y_data = np.load(cube_y_path)
-        assert cube_x_data.shape == cube_y_data.shape
-        len_z = cube_x_data.shape[1]
+        len_z = cube_x_data.shape[2]
         y_hat = np.zeros(cube_y_data.shape)
         
         for idx in range(len_z):
 
-            batch_x = np.zeros((1, 3, cube_x_data.shape[0], cube_x_data.shape[2]))
-            batch_y = cube_y_data[:, idx, :]
+            batch_x = np.zeros((1, 3, cube_x_data.shape[0], cube_x_data.shape[1]))
+            # batch_y = cube_y_data[:, idx, :]
 
             z_center = idx
-            batch_x[0, 1, :, :] = cube_x_data[:, z_center, :]
+            batch_x[0, 1, :, :] = cube_x_data[:, :, z_center]
             z_before = z_center - 1 if z_center > 0 else 0
             z_after = z_center + 1 if z_center < len_z-1 else len_z-1
-            batch_x[0, 0, :, :] = cube_x_data[:, z_before, :]
-            batch_x[0, 2, :, :] = cube_x_data[:, z_after, :]
+            batch_x[0, 0, :, :] = cube_x_data[:, :, z_before]
+            batch_x[0, 2, :, :] = cube_x_data[:, :, z_after]
 
             batch_x = torch.from_numpy(batch_x).float().to(device)
 
             y_hat_output = model(batch_x).cpu().detach().numpy()
-            y_hat[:, idx, :] = np.squeeze(y_hat_output[:, 1, :, :])
+            y_hat[:, :, idx] = np.squeeze(y_hat_output[:, 1, :, :])
         
         for cnt_loss, loss_fnc in enumerate(criterion_list):
             curr_loss = loss_fnc(cube_y_data, y_hat).item()
             loss_mat[cnt_X, cnt_loss] = curr_loss
             print("===> Loss[{}]: {:6}".format(loss_fnc.__name__, curr_loss), end='')
         
-        # file_idx = os.path.basename(X_path)[4:7]
-        # nifty_name = "mets" if file_idx[0] == "0" else "tami"
-        # nifty_name = nifty_name + "000" + file_idx[1:] + ".nii.gz"
-        # nifty_name = "./t1map2bravo/BRAVO/" + nifty_name
-        # nifty_file = nib.load(nifty_name)
-        # print("Loaded from", nifty_name, end="")
-
-        # pred_file = nib.Nifti1Image(y_hat*7000, nifty_file.affine, nifty_file.header)
-        # pred_name = "./t1map2bravo/pred/"+"PRD_"+file_idx+".nii.gz"
-        # nib.save(pred_file, pred_name)
-        # print(" Saved to", pred_name)
-
-        nifty_name = "./MR2CT_S/MRS/" + os.path.basename(X_path)[:-4]+".nii.gz"
+        nifty_name = "./MR2CT/ct_bravo/CT__MLAC_" + os.path.basename(X_path)[4:6]+"_MNI.nii.gz"
         nifty_file = nib.load(nifty_name)
         print("Loaded from", nifty_name, end="")
 
-        pred_file = nib.Nifti1Image(y_hat*1500, nifty_file.affine, nifty_file.header)
-        pred_name = "./MR2CT_S/pred/"+"PRD_"+os.path.basename(X_path)[4:7]+".nii.gz"
+
+        pred_file = nib.Nifti1Image(y_hat, nifty_file.affine, nifty_file.header)
+        pred_name = "./MR2CT_B_UNET/pred/"+"PRD_"+os.path.basename(X_path)[4:7]+".nii.gz"
         nib.save(pred_file, pred_name)
         print(" Saved to", pred_name)
 
