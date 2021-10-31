bf042ac0e63b6c3d57f338042e72ea21e6953d47
WeijieChen2017 <weijiechen1997@gmail.com>
2021-10-31 00:04:40 -0500

    Good morning!

--- SwinIR/rename.py
+++ SwinIR/rename.py
@@ -4,8 +4,8 @@ for idx in range(10):
     name_mri = "MR__MLAC_"+str(idx)+"_MNI.nii.gz"
     name_sct_new = "CT__MLAC_{:02d}+_MNI.nii.gz".format(idx)
     name_mri_new = "MR__MLAC_{:02d}+_MNI.nii.gz".format(idx)
-    cmd_mri = "mv ./t1_bravo/"+name_mri+" ./t1_bravo/"+name_mri_new
-    cmd_sct = "mv ./ct_bravo/"+name_sct+" ./ct_bravo/"+name_sct_new
+    cmd_mri = "mv ./MR2CT/t1_bravo/"+name_mri+" ./MR2CT/t1_bravo/"+name_mri_new
+    cmd_sct = "mv ./MR2CT/ct_bravo/"+name_sct+" ./MR2CT/ct_bravo/"+name_sct_new
     print(cmd_mri)
     os.system(cmd_mri)
     print(cmd_sct)
