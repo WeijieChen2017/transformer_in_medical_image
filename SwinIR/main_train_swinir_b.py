881f41d8e2d6ce9228e73a9d3abd9e3108add9a8
WeijieChen2017 <weijiechen1997@gmail.com>
2021-11-01 01:18:59 -0500

    Good morning!

--- SwinIR/main_train_swinir_b.py
+++ SwinIR/main_train_swinir_b.py
@@ -9,6 +9,7 @@ import torch.nn as nn
 
 from models.network_swinir import SwinIR as net
 from models_deit import DistilledVisionTransformer
+from functools import partial
 from utils import util_calculate_psnr_ssim as util
 from unet import UNet
 
