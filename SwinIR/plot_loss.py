d9f1124594d7a1bf4da0e4a70c241ef339e947c3
WeijieChen2017 <weijiechen1997@gmail.com>
2021-11-02 11:16:31 -0500

    Good morning!

--- SwinIR/plot_loss.py
+++ SwinIR/plot_loss.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 
-folder = "./xue_5to1/npy/"
+folder = "./xue_5to1/"
 
-npy_list = sorted(glob.glob(folder+"epoch_loss_*.npy"))
+npy_list = sorted(glob.glob(folder+"npy/epoch_loss_*.npy"))
 for npy_path in npy_list:
     print(npy_path)
 
@@ -14,8 +14,8 @@ loss_v = np.zeros((n_epoch))
 
 for idx in range(n_epoch):
     num = "{:03d}".format(idx+1)
-    name_t = folder+"epoch_loss_t_{}.npy".format(num)
-    name_v = folder+"epoch_loss_v_{}.npy".format(num)
+    name_t = folder+"npy/epoch_loss_t_{}.npy".format(num)
+    name_v = folder+"npy/epoch_loss_v_{}.npy".format(num)
     data_t = np.load(name_t)
     data_v = np.load(name_v)
     loss_t[idx] = np.mean(data_t)
