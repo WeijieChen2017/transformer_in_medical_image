6152a849f5913a32eb0a8111bbb51df2a01d9d8a
WeijieChen2017 <weijiechen1997@gmail.com>
2021-11-02 02:23:06 -0500

    Good morning!

---
+++ SwinIR/data_investigation_xue.py
@@ -0,0 +1,22 @@
+import os
+import glob
+import nibabel as nib
+import numpy as np
+
+modality_hub = ["CTAC", "InPhase", "OutPhase", "FAT", "NAC", "WATER"]
+
+for modality in modality_hub:
+    print("-"*60)
+    file_list = sorted(glob.glob("./xue/sub*/*{}.nii.gz".format(modality)))
+    max_list = np.zeros(len(file_list))
+    min_list = np.zeros(len(file_list))
+    cnt = 0
+    for file_path in file_list:
+        print("--->",os.path.basename(filename),"<---",end="")
+        data = nib.load(file_path).get_fdata()
+        max_list[cnt] = np.amax(data)
+        min_list[cnt] = np.amin(data)
+        print(" Max:", max_list[cnt], "Min: ", min_list[cnt])
+        cnt += 1
+    print("Max max:", np.amax(max_list), "Min min: ", np.amin(min_list))
+
