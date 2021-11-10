4030985aa6ef5cc08a3a5c7cef30929d0cda8d66
WeijieChen2017 <weijiechen1997@gmail.com>
2021-11-10 10:15:32 -0600

    Good morning!

--- SwinIR/cmd_3dresample.py
+++ SwinIR/cmd_3dresample.py
@@ -1,8 +1,8 @@
 import os
 import glob
 
-for idx in range(64):
-    cmd = "3dresample -dxyz 0.875 0.875 1 -rmode Cu -prefix 224_{:03d}.nii.gz ".format(idx)
-    cmd += "-input CT__MLAC_{:02d}".format(idx)+"_MNI.nii.gz" 
+for idx in range(100):
+    cmd = "3dresample -dxyz 0.8594 0.8594 1 -rmode Cu -prefix 256_{:03d}.nii.gz ".format(idx)
+    cmd += "-input CT__MLAC_{:02d}".format(idx)+"_ORIG.nii.gz" 
     print(cmd)
     os.system(cmd)
