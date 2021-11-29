f8a2d730edcb5c29b04e7eae8051caff6ba4af33
WeijieChen2017 <weijiechen1994@gmail.com>
2021-11-28 18:06:44 -0600

    Good morning!

--- SwinIR/plot_loss.py
+++ SwinIR/plot_loss.py
@@ -1,33 +1,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
+import copy
 
 n_epoch = 50
-folder = "./MR2CT_B_SWINIR_11-5/"
+folder = "./bridge_small/*/"
 
-npy_list = sorted(glob.glob(folder+"npy/epoch_loss_*.npy"))
+model_hub = []
+npy_list = sorted(glob.glob(folder+"loss/epoch_loss_*.npy"))
 for npy_path in npy_list:
-    print(npy_path)
+    # print(npy_path)
+    path_split = npy_path.split("/")
+    model_name = path_split[2]+"_"+path_split[4][-9]
+    # print(model_name)
+    if not model_name in model_hub:
+        model_hub.append(model_name)
+print(model_hub)
 
-loss_t = np.zeros((n_epoch))
-loss_v = np.zeros((n_epoch))
+loss = np.zeros((n_epoch))
+plot_target = []
+for model_name in model_hub:
+    current_package = [model_name]
+    for idx in range(n_epoch):
+        num = "{:03d}".format(idx+1)
+        name = "./bridge_small/{}/loss/epoch_loss_{}_{}.npy".format(model_name[:-2], model_name[-1], num)
+        data = np.load(name)
+        loss[idx] = np.mean(data)
+    current_package.append(copy.deepcopy(loss))
 
-for idx in range(n_epoch):
-    num = "{:03d}".format(idx+1)
-    name_t = folder+"npy/epoch_loss_t_{}.npy".format(num)
-    name_v = folder+"npy/epoch_loss_v_{}.npy".format(num)
-    data_t = np.load(name_t)
-    data_v = np.load(name_v)
-    loss_t[idx] = np.mean(data_t)
-    loss_v[idx] = np.mean(data_v)
 
+legend_list = []
 plt.figure(figsize=(9,6), dpi=300)
-plt.plot(range(n_epoch), loss_t)
-plt.plot(range(n_epoch), loss_v)
+for package in plot_target:
+    loss_array = package[1]
+    loss_tag = package[0]
+    legend_list.append(loss_tag)
+    plt.plot(range(n_epoch), loss_array)
+
 plt.xlabel("epoch")
 plt.ylabel("loss")
 plt.yscale("log")
 plt.legend(["training", "validation"])
 plt.title("Training curve")
 
-plt.savefig(folder+"loss_{}.jpg".format(n_epoch))
+plt.savefig("./bridge_small/loss_{}.jpg".format(n_epoch))
