a00f723f79fe4f34217072e2d876acabcf1bde58
WeijieChen2017 <weijiechen1994@gmail.com>
2021-12-07 12:45:20 -0600

    Good morning!

--- SwinIR/main_test_unet_bridge_small.py
+++ SwinIR/main_test_unet_bridge_small.py
@@ -43,7 +43,7 @@ def main():
     parser.add_argument('--gpu_ids', type=str, default="6", help='Use which GPU to train')
     parser.add_argument('--folder_X_te', type=str, default="./bridge_3000/X/test/", help='input folder of T1MAP PET images')
     parser.add_argument('--folder_Y_te', type=str, default="./bridge_3000/Y/test/", help='input folder of BRAVO images')
-    parser.add_argument('--root_folder', type=str, default="./bridge_3000/naive_aug/", help='input folder of BRAVO images')
+    parser.add_argument('--root_folder', type=str, default="./bridge_3000/naive_intra_aug/", help='input folder of BRAVO images')
     parser.add_argument('--weights_path', type=str, default='model_best_046.pth')
     args = parser.parse_args()
 
