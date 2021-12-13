6bf441d7419cf484cde84a5782f9413041f9e466
WeijieChen2017 <weijiechen1994@gmail.com>
2021-12-13 02:37:25 -0600

    Good morning!

--- SwinIR/ImgGen/models.py
+++ SwinIR/ImgGen/models.py
@@ -7,13 +7,13 @@ class ConvTransUnet(nn.Module):
         super(ConvTransUnet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
-        self.conv1 = ConvTrans(n_channels, 128)
-        self.conv2 = ConvTrans(128, 128)
-        self.conv3 = ConvTrans(128, 128)
-        self.conv4 = ConvTrans(128, 128)
-        self.conv5 = ConvTrans(128, 128)
+        self.conv1 = ConvTrans(n_channels, 80)
+        self.conv2 = ConvTrans(80, 80)
+        self.conv3 = ConvTrans(80, 80)
+        self.conv4 = ConvTrans(80, 80)
+        self.conv5 = ConvTrans(80, 80)
         # self.conv6 = ConvTrans(self.width, 256, self.width)
-        self.outc = OutConv(128, n_classes)
+        self.outc = OutConv(80, n_classes)
 
     def forward(self, x):
         x = self.conv1(x)
