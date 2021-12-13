1021fd27eb894149b81f8da21f2c48003a770cc4
WeijieChen2017 <weijiechen1994@gmail.com>
2021-12-12 23:31:14 -0600

    Good morning!

--- SwinIR/ImgGen/modules.py
+++ SwinIR/ImgGen/modules.py
@@ -33,13 +33,13 @@ class ConvTrans(nn.Module):
 
         self.conv1 = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0),
-            nn.InstanceNorm2d(mid_channels),
+            nn.InstanceNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
 
         self.conv2 = nn.Sequential(
             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
-            nn.InstanceNorm2d(mid_channels),
+            nn.InstanceNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
 
@@ -48,7 +48,7 @@ class ConvTrans(nn.Module):
             nn.InstanceNorm2d(mid_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
-            nn.InstanceNorm2d(mid_channels),
+            nn.InstanceNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
 
@@ -60,7 +60,7 @@ class ConvTrans(nn.Module):
             nn.InstanceNorm2d(mid_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
-            nn.InstanceNorm2d(mid_channels),
+            nn.InstanceNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
 
