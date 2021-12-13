13b3308a270261635534b074e6a4de4472232dea
WeijieChen2017 <weijiechen1994@gmail.com>
2021-12-13 02:01:05 -0600

    Good morning!

--- SwinIR/main_train_unet_bridge_small.py
+++ SwinIR/main_train_unet_bridge_small.py
@@ -79,8 +79,8 @@ def main():
 
     parser.add_argument('--input_channel', type=int, default=1, help='the number of input channel')
     parser.add_argument('--output_channel', type=int, default=1, help='the number of output channel')
-    parser.add_argument('--tag', type=str, default="./bridge_3000/naive_ConvTrans5_flatten_huber/", help='Save_prefix')
-    parser.add_argument('--gpu_ids', type=str, default="5", help='Use which GPU to train')
+    parser.add_argument('--tag', type=str, default="./bridge_3000/naive_ConvTrans5_flatten_mse/", help='Save_prefix')
+    parser.add_argument('--gpu_ids', type=str, default="4", help='Use which GPU to train')
     parser.add_argument('--epoch', type=int, default=50, help='how many epochs to train')
     parser.add_argument('--batch', type=int, default=4, help='how many batches in one run')
     parser.add_argument('--loss_display_per_iter', type=int, default=600, help='display how many losses per iteration')
@@ -105,8 +105,8 @@ def main():
     model = ConvTransUnet(n_channels=input_channel, n_classes=output_channel)
     model.train().float()
     model = model.to(device)
-    criterion = nn.SmoothL1Loss()
-    # criterion = nn.MSELoss()
+    # criterion = nn.SmoothL1Loss()
+    criterion = nn.MSELoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
 
     sct_list = sorted(glob.glob(args.folder_sct+"*.nii.gz"))
