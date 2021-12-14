66ff1e0013e223fafdc9006beaa0d7f15427bb06
WeijieChen2017 <weijiechen1994@gmail.com>
2021-12-14 02:14:30 -0600

    Good morning!

--- SwinIR/ImgGen/modules.py
+++ SwinIR/ImgGen/modules.py
@@ -15,7 +15,7 @@ class OutConv(nn.Module):
         self.conv = nn.Sequential(
             nn.Conv2d(in_channels, in_channels, kernel_size=1, padding=0),
             nn.InstanceNorm2d(out_channels),
-            nn.ReLU(inplace=True)
+            nn.ReLU(inplace=True),
             nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0),
         )
 
